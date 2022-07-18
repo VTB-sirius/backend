7818c2f9aa8eee3a0cb01b18c8348e65a731f272
Кирилл Осинцев <osintsev.2000@inbox.ru>
2022-07-19 02:03:47 +0300

    connected to db. add code for run model

--- app/views/http/projects.py
+++ app/views/http/projects.py
@@ -1,16 +1,17 @@
 from random import randint
+from bson import ObjectId
 from fastapi import APIRouter, HTTPException, UploadFile
 
-import requests
+import subprocess
 
 from app.adapters.ya import yc
 
 from .documents import router as doc_router
 from ...settings import s3, db
 
+
 router = APIRouter(prefix="/projects")
 router.include_router(doc_router)
-
 
 
 @router.post("/", status_code=201)
@@ -25,23 +26,23 @@ async def create_project(file: UploadFile):
 	project_id = db['projects'].insert_one({
 		'file_id': fileKey,
 	}).inserted_id
-
-	r = requests.post('https://datasphere.api.cloud.yandex.net/datasphere/v1/nodes/7fbc3783-bd8a-482e-b1cb-6c78dcd327c7:execute', json={
-		'folder_id': 'b1g59nv1s9n6s73toofh',
-		'node_id': '7fbc3783-bd8a-482e-b1cb-6c78dcd327c7',
-		'input': {
-			'project_id': project_id,
-			'file_id': fileKey,
-		}
-	}, headers={
-		'Authorization': 'Bearer ' + yc.aim_token
-	})
-
-	print(r.status_code)
 	
+	subprocess.run("docker run -it --rm --name dbs_model --env-file /.env.docker -v /home/simbauser/sirius/models/:/home/jupyter deploymodel:latest python3 /home/jupyter/model_dbs.py", project_id, fileKey)
+		
 	return {"status": "success", "payload": {"id": str(project_id)}}
 
 
-@router.get("/{id}")
-async def get_project(id: int):
-   raise HTTPException(status_code=404, detail="not found")
+@router.get("/{_id}")
+async def get_project(_id: str, model: str):
+	project = db['projects'].find_one({ '_id': ObjectId(_id) })
+
+	if model == 'lda' and ('lda_payload' in project):
+		return { "status": "success", "payload": project['lda_payload'] }
+	elif model == 'lda':
+		return { "status": "pending", "payload": {} }
+	elif model == 'dbscan' and ('dbs_payload' in project):
+		return { "status": "success", "payload": project['dbs_payload'] }
+	elif model == 'dbscan':
+		return { "status": "pending", "payload": {} }
+	else:
+		raise HTTPException(status_code=404, detail="not found")

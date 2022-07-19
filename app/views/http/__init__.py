4514f1f596704f1fe9891d370d80531bf072a92a
Кирилл Осинцев <osintsev.2000@inbox.ru>
2022-07-19 14:32:25 +0300

    added router

--- app/views/http/__init__.py
+++ app/views/http/__init__.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 
 from .projects import router as pr_router
+from .documents import router as doc_router
 
 router = APIRouter()
 router.include_router(pr_router)
+router.include_router(doc_router)

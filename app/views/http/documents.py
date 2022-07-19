9f722c20043ffb7571ea4b25f2a1324837bd79ea
Кирилл Осинцев <osintsev.2000@inbox.ru>
2022-07-19 14:39:17 +0300

    fixed

--- app/views/http/documents.py
+++ app/views/http/documents.py
@@ -1,6 +1,5 @@
 from concurrent.futures import process
 from random import randint
-from bson import ObjectId
 from fastapi import APIRouter, HTTPException, UploadFile
 
 from app.adapters.ya import yc
@@ -10,7 +9,7 @@ from ...settings import db
 router = APIRouter(prefix="/documents")
 
 @router.get("/{_id}")
-async def get_project(_id: str):
-	doc = db['documents'].find_one({ '_id': ObjectId(_id) })
+async def get_document(_id: str):
+	doc = db['documents'].find_one({ '_id': _id })
 	
 	return { 'status': 'success', 'payload': doc }

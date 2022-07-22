c97450ecb8caa945e8968843f44f28c98baadae4
Кирилл Осинцев <osintsev.2000@inbox.ru>
2022-07-22 23:02:25 +0300

    updated cors

--- app/__init__.py
+++ app/__init__.py
@@ -10,7 +10,7 @@ app.include_router(router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://classify.ml", "http://classify.ml"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
